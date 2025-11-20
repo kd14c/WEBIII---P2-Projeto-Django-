a4c0219d1eaddfc00e2d7d8a8b200e25b5b6cce0
kd14c <carlos.campolongo@fatec.sp.gov.br>
2025-11-20 20:03:04 -0300

    Add pedido and perfil templates with purchase functionality

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Pagina, Produto
 
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Produto, Pedido
+
+
 def index(request):
     pagina = Pagina.objects.first()
     produtos = Produto.objects.all()
@@ -8,4 +13,38 @@ def index(request):
     return render(request, 'core/index.html', {
         'pagina': pagina,
         'produtos': produtos
+    })
+
+
+@login_required
+def comprar(request, id):
+    produto = get_object_or_404(Produto, id=id)
+
+    if request.method == 'POST':
+        quantidade = int(request.POST.get('quantidade'))
+        total = quantidade * produto.preco
+
+        produto.estoque -= quantidade
+        produto.save()
+
+        Pedido.objects.create(
+            usuario=request.user,
+            produto=produto,
+            quantidade=quantidade,
+            total=total
+        )
+
+        return redirect('perfil')
+
+    return render(request, 'core/pedido.html', {
+        'produto': produto
+    })
+
+
+@login_required
+def perfil(request):
+    pedidos = Pedido.objects.filter(usuario=request.user).order_by('-data')
+
+    return render(request, 'core/perfil.html', {
+        'pedidos': pedidos
     })
